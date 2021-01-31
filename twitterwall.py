79a48eb5822d4ae36c574867ee0cc3a0c15df541
Meet Patel <35294311+zedxin0@users.noreply.github.com>
2021-01-31 16:17:37 +0530

    Add files via upload

---
+++ twitterwall.py
@@ -0,0 +1,59 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json, ast
+
+import twitter_credentials
+ 
+#=#=#=# TWITTER STREAMER #=#=#=#
+class TwitterStreamer():
+
+    def __init__(self):
+        pass
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        stream = Stream(auth, listener)
+
+
+        stream.filter(track=hash_tag_list, follow=users)
+
+
+#=#=#=# TWITTER STREAM LISTENER #=#=#=#
+class StdOutListener(StreamListener):
+
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+
+            data_dict = json.loads(data)
+            print(data_dict['user']['screen_name'])
+            print("_________________")
+            print(data_dict['text'])
+            print("________________________________________")
+            #with open(self.fetched_tweets_filename, 'a') as tf:
+            #    tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+          
+
+    def on_error(self, status):
+        print(status)
+
+ 
+if __name__ == '__main__':
+ 
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["#tesla", "#elonmusk", "#spacex"]
+    users = [] # id of user 'meetofleaf'
+    fetched_tweets_filename = "tweets.json"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
